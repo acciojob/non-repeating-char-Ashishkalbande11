9a8ecaca75307ac37482c241eea8583ff11bbcc1
Ashishkalbande11 <154236313+Ashishkalbande11@users.noreply.github.com>
2024-07-18 08:44:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function firstNonRepeatedChar(str) {
- // Write your code here
+function firstNonRepeatedChar(s) {
+ // Write your code here\
+	let map = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        let char = s.charAt(i);
+        map.set(char, (map.get(char) || 0) + 1);
+    }
+    
+    for (let i = 0; i < s.length; i++) {
+        if (map.get(s.charAt(i)) === 1) {
+            return s.charAt(i);
+        }
+    }
+    
+    return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
